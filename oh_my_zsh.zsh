3abc0809a6a0e392ce26f547276c20917e41a05a
Ming Han Teh <teh_minghan@hotmail.com>
2012-07-04 15:17:05 -0700

    Added useful plugins

--- oh_my_zsh.zsh
+++ oh_my_zsh.zsh
@@ -21,7 +21,7 @@ DISABLE_AUTO_TITLE="true"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git extract)
+plugins=(git extract mvn ant)
 
 source $ZSH/oh-my-zsh.sh
 
