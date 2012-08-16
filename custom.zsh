ae49a7f91c9c185b8781eba9643167f62ef1024a
minghan <teh_minghan@hotmail.com>
2012-08-16 01:36:52 -0700

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -1,4 +1,3 @@
-
 # ============================
 # Alias
 
@@ -9,6 +8,9 @@ alias git-addremove="git add . ; git add -u"
 
 grep_() { grep $@ -r -i --color='always' * }
 find_() { find ./ -name "*$@*" }
+
+psgrep_() { ps ax | grep -i "$@" | grep -v grep }
+pskill_() { ps ax | grep -i "$@" | grep -v grep | awk '{print $1}' | xargs kill -SIGKILL }
 
 alias gcc_="gcc -ansi -pedantic -W -Wextra -Wall -g"
 alias valgrind_="valgrind --tool=memcheck --leak-check=yes --show-reachable=yes"
